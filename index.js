7b288daf1aff8f1e6af5c7b9cfbaaa0d490d95c7
HBazai <bazaee.hassan@gmail.com>
2023-09-07 23:27:25 +0330

    Lesson-Twelve check responsive of websites

--- index.js
+++ index.js
@@ -4,22 +4,31 @@ puppeteer.use(StealthPlugin()); // enable stealth mode
 const fs = require("fs");
 
 const ppr = require("puppeteer");
-const device = ppr.KnownDevices["Galaxy S III"]
+
+const devices = {
+    device_1: ppr.KnownDevices["iPhone 8 landscape"],
+    device_3: ppr.KnownDevices["BlackBerry Z30 landscape"],
+    device_4: ppr.KnownDevices["Galaxy S9+"],
+    device_5: ppr.KnownDevices["Galaxy Tab S4 landscape"],
+    device_6: ppr.KnownDevices["JioPhone 2 landscape"]
+}
 
 
 async function simulateMobileDevice(url) {
     try {
         const browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe' // specify the path of your Chrome browser
         });
         const page = await browser.newPage();
-        
+
         /***Do what ever you want here */
-        await page.emulate(device);
-        
-        await page.goto(url);
-        
+        for (var device in devices) { // loop through the keys of the devices object
+            await page.emulate(devices[device]);
+            await page.goto (url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+            await page.screenshot({ path: `${devices[device].name}.png`, fullPage: 'true' });
+        }
+
         await browser.close();
 
         console.log('Success!')
@@ -28,7 +37,7 @@ async function simulateMobileDevice(url) {
     }
 }
 
-const url = 'https://tradingview.com';
+const url = 'https://vuetifyjs.com/en/';
 
 simulateMobileDevice(url);
 
