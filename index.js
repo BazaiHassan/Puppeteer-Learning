ce9a79bfe1afdbf293043b0692cedb3a56dab53b
HBazai <bazaee.hassan@gmail.com>
2023-09-07 13:30:44 +0330

    Lesson-Five Generate PDF

--- index.js
+++ index.js
@@ -1,67 +1,22 @@
 const puppeteer = require("puppeteer");
-const fs = require("fs")
 
-async function run() {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
+async function generatePDF(url, outputFile) {
+    try {
+        const browser = await puppeteer.launch({ headers: false, headless: true });
+        const page = await browser.newPage();
 
-    //Navigate to the website
-    await page.goto('https://uk.yahoo.com/?p=us&guccounter=2&guce_referrer=aHR0cHM6Ly9jb25zZW50LnlhaG9vLmNvbS8&guce_referrer_sig=AQAAAEdJABbi9VQgceiqzDoen3VM2DzSCdiW6H91DmwOpNcM8MIVegMR4WHUt4UCdZJUjZd1TkrinkttqPVFUtRYsMlx1qUnhhe0cZSzjz-vPVAzOz20m4rXj9TQ66-cEOstnBRWstNkAoq-gdQ7laS2qfb-TdUaWgHB3jtQp-Qf8Wpv', { waitUntil: 'load', timeout: 0 });
+        await page.goto(url)
+        await page.pdf({
+            path: outputFile,
+            format: 'A4'
+        });
+        await browser.close();
+    } catch (error) {
 
-    // //Wait for the cookie popup to appear
-    // await page.waitForSelector('.consent-form', {visible: true});
-
-    // //Click on the accept button
-    // await page.click('button[type="submit"][name="agree"]');
-
-    //SEO collecting data
-    const title = await page.title();
-
-    // Wait for the meta tags to be rendered
-    await page.waitForSelector('meta[name="description"]');
-    await page.waitForSelector('meta[name="keywords"]');
-
-    const metaDescription = await page.$eval('meta[name="description"]', (element) => element.content);
-
-    const metaKeywords = await page.$eval('meta[name="keywords"]', (element) => element.content);
-
-    // Extract links
-    const links = await page.$$eval("a", (elements) => elements.map((element) => ({
-        href: element.href,
-        text: element.textContent
-    })));
-
-    // Extract images
-    const images = await page.$$eval("img", (elements) =>
-        elements.map((element) => ({
-            src: element.src,
-            alt: element.alt
-        }))
-    );
-
-    const imagesCount = images.length;
-    const linksCount = links.length;
-
-    // Prepare output format
-    const outputData = {
-        title,
-        metaDescription,
-        metaKeywords,
-        images,
-        links,
-        imagesCount,
-        linksCount
-    };
-
-    // Convert JSON to string
-    const outputJSON = JSON.stringify(outputData);
-
-    // write to a file
-    fs.writeFileSync("output.json", outputJSON);
-
-    await browser.close();
-
-
+    }
 }
 
-run()
+const url = 'http://google.com';
+const outputFile = 'output.pdf';
+
+generatePDF(url, outputFile);
