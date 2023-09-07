8d3a586197c441bcf84eb32bd93cdf2664d0c984
HBazai <bazaee.hassan@gmail.com>
2023-09-07 10:20:39 +0330

    Lesson-Two Puppeteer

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const puppeteer = require("puppeteer");
 
-(async () => {
-    try {
-        const browser = await puppeteer.launch({
-            headless: false
-        });
+async function run() {
+    // Launch the browser instance
+    const browser = await puppeteer.launch({
+        headless: false
+    });
 
-        const page = await browser.newPage();
+    const page = await browser.newPage();
 
-        await page.goto('https://google.com');
-        await page.screenshot({ path: 'google.png' });
-        await browser.close();
-    } catch (error) {
-        console.log(error);
-    }
-})();
+    await page.goto("https://mlmond.com")
+    const title = await page.title();
+    console.log(title);
+
+    // Scrap all h1 tags
+    const heading = await page.$eval('h1', (element) => element.textContent);
+    console.log(heading);
+
+    await page.screenshot({ path: 'mlmond.png' });
+
+    await page.pdf({ path: 'mlmond.pdf' });
+
+    await browser.close();
+}
+
+run();
