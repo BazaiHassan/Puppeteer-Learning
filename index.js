034481283615df0f429c587df46c612ba82f747f
HBazai <bazaee.hassan@gmail.com>
2023-09-07 17:44:03 +0330

    Lesson-Seven Zillow.com

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra"); // require puppeteer-extra
+const StealthPlugin = require("puppeteer-extra-plugin-stealth"); // require stealth plugin
+puppeteer.use(StealthPlugin()); // enable stealth mode
+const fs = require("fs");
 
-async function generateScreenShot(url, outputFile) {
+async function getSourceCode(url, outputData){
     try {
-        const browser = await puppeteer.launch({ headless: true });
+        const browser = await puppeteer.launch({
+            headless:true,
+            executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe' // specify the path of your Chrome browser
+        });
         const page = await browser.newPage();
+        await page.goto(url, {timeout: 60000});
 
-        await page.goto(url)
-        await page.screenshot({ path: outputFile })
+        const sourceCode = await page.content();
+
+        fs.writeFileSync(outputData, sourceCode, 'utf-8');
+
         await browser.close();
-        console.log("Success!")
+
+        console.log('Success!')
     } catch (error) {
-        console.log("Unable to generate screenshot")
-        console.log(error.message)
+        console.log('Error: ', error.message);
     }
 }
 
 const url = 'https://zillow.com';
-const outputFile = 'output.png';
+const outputData = 'source.html';
 
-generateScreenShot(url, outputFile);
+getSourceCode(url, outputData)
